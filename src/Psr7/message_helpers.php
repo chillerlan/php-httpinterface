8fa47928586298e8058935b421faa0ae99fac535
codemasher <smiley@chillerlan.net>
2018-09-08 20:02:27 +0200

    :octocat:

--- src/Psr7/message_helpers.php
+++ src/Psr7/message_helpers.php
@@ -10,8 +10,7 @@
 namespace chillerlan\HTTP\Psr7;
 
 use InvalidArgumentException;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\{MessageInterface, RequestInterface, ResponseInterface, UploadedFileInterface};
 
 const PSR7_INCLUDES = true;
 
@@ -296,4 +295,35 @@ function get_json(ResponseInterface $response, bool $assoc = null){
  */
 function get_xml(ResponseInterface $response){
 	return simplexml_load_string($response->getBody()->getContents());
+}
+
+/**
+ * Returns the string representation of an HTTP message. (from Guzzle)
+ *
+ * @param MessageInterface $message Message to convert to a string.
+ *
+ * @return string
+ */
+function message_to_string(MessageInterface $message){
+
+	if($message instanceof RequestInterface){
+		$msg = trim($message->getMethod().' '.$message->getRequestTarget()).' HTTP/'.$message->getProtocolVersion();
+
+		if(!$message->hasHeader('host')){
+			$msg .= "\r\nHost: ".$message->getUri()->getHost();
+		}
+
+	}
+	elseif($message instanceof ResponseInterface){
+		$msg = 'HTTP/'.$message->getProtocolVersion().' '.$message->getStatusCode().' '.$message->getReasonPhrase();
+	}
+	else{
+		throw new \InvalidArgumentException('Unknown message type');
+	}
+
+	foreach($message->getHeaders() as $name => $values){
+		$msg .= "\r\n{$name}: ".implode(', ', $values);
+	}
+
+	return "{$msg}\r\n\r\n".$message->getBody();
 }
