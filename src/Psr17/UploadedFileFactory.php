0a8e21354a5c96a3f6a634daa2ae99aa2f035af5
codemasher <smiley@chillerlan.net>
2019-03-13 15:38:56 +0100

    :wrench: fix UploadedFileFactory $size according to https://github.com/http-interop/http-factory-tests

--- src/Psr17/UploadedFileFactory.php
+++ src/Psr17/UploadedFileFactory.php
@@ -39,7 +39,7 @@ final class UploadedFileFactory implements UploadedFileFactoryInterface{
 	 * @throws \InvalidArgumentException If the file resource is not readable.
 	 */
 	public function createUploadedFile(StreamInterface $stream, int $size = null, int $error = \UPLOAD_ERR_OK, string $clientFilename = null, string $clientMediaType = null):UploadedFileInterface{
-		return new UploadedFile($stream, $size, $error, $clientFilename, $clientMediaType);
+		return new UploadedFile($stream, $size ?? $stream->getSize(), $error, $clientFilename, $clientMediaType);
 	}
 
 }
