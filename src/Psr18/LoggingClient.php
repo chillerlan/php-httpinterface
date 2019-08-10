a03528770830183c6bf884f23ecf86843f6ac336
codemasher <smiley@chillerlan.net>
2019-08-10 10:20:35 +0200

    :octocat: +LoggingClient

---
+++ src/Psr18/LoggingClient.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Class LoggingClient
+ *
+ * a silly logging wrapper (do not use in production!)
+ *
+ * @filesource   LoggingClient.php
+ * @created      07.08.2019
+ * @package      chillerlan\HTTP\Psr18
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2019 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\HTTP\Psr18;
+
+use chillerlan\HTTP\Psr17\ResponseFactory;
+use Psr\Http\Client\{ClientExceptionInterface, ClientInterface};
+use Psr\Http\Message\{RequestInterface, ResponseFactoryInterface, ResponseInterface};
+use Psr\Log\{LoggerAwareInterface, LoggerAwareTrait, LoggerInterface, NullLogger};
+use Throwable;
+
+use function chillerlan\HTTP\Psr7\message_to_string;
+use function get_class;
+
+class LoggingClient implements ClientInterface, LoggerAwareInterface{
+	use LoggerAwareTrait;
+
+	/** @var \Psr\Http\Client\ClientInterface */
+	protected $http;
+	/** @var \Psr\Http\Message\ResponseFactoryInterface|null */
+	protected $responseFactory;
+
+	/**
+	 * LoggingClient constructor.
+	 *
+	 * @param \Psr\Http\Client\ClientInterface                $http
+	 * @param \Psr\Log\LoggerInterface|null                   $logger
+	 * @param \Psr\Http\Message\ResponseFactoryInterface|null $responseFactory
+	 */
+	public function __construct(ClientInterface $http, LoggerInterface $logger = null, ResponseFactoryInterface $responseFactory = null){
+		$this->http            = $http;
+		$this->logger          = $logger ?? new NullLogger;
+		$this->responseFactory = $responseFactory ?? new ResponseFactory;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function sendRequest(RequestInterface $request):ResponseInterface{
+		$this->logger->debug("\n----HTTP-REQUEST----\n".message_to_string($request));
+
+		try{
+			$response = $this->http->sendRequest($request);
+		}
+		catch(Throwable $e){
+			$this->logger->debug("\n----HTTP-ERROR------\n".message_to_string($request));
+			$this->logger->error($e->getMessage());
+			$this->logger->error($e->getTraceAsString());
+
+			if(!$e instanceof ClientExceptionInterface){
+				throw new ClientException('unexpected exception, does not implement "ClientExceptionInterface": '.get_class($e));
+			}
+
+			/** @noinspection PhpUnhandledExceptionInspection */
+			throw $e;
+		}
+
+		$this->logger->debug("\n----HTTP-RESPONSE---\n".message_to_string($response));
+
+		return $response;
+	}
+
+}
---
+++ tests/Psr18/LoggingClientTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Class LoggingClientTest
+ *
+ * @filesource   LoggingClientTest.php
+ * @created      10.08.2019
+ * @package      chillerlan\HTTPTest\Psr18
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2019 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\HTTPTest\Psr18;
+
+use chillerlan\HTTP\Psr18\LoggingClient;
+use Psr\Log\AbstractLogger;
+
+class LoggingClientTest extends CurlClientTest{
+
+	protected function setUp():void{
+		parent::setUp();
+
+		$logger = new class() extends AbstractLogger{
+			public function log($level, $message, array $context = []){
+				echo sprintf('[%s][%s] %s', date('Y-m-d H:i:s'), $level, trim($message))."\n";
+			}
+		};
+
+		$this->http = new LoggingClient($this->http, $logger);
+	}
+
+	/**
+	 * @dataProvider requestDataProvider
+	 *
+	 * @param $method
+	 * @param $extra_headers
+	 */
+	public function testRequest(string $method, array $extra_headers){
+		$this->markTestSkipped('N/A');
+	}
+
+}
