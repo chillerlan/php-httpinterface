40d490595d2464f4df83d9c152a43ab052eb293a
codemasher <smiley@chillerlan.net>
2019-08-14 06:13:26 +0200

    :octocat: WIP

---
+++ tests/CurlUtils/CurlMultiClientTest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Class CurlMultiClientTest
+ *
+ * @filesource   CurlMultiClientTest.php
+ * @created      11.08.2019
+ * @package      chillerlan\HTTPTest\CurlUtils
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2019 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\HTTPTest\CurlUtils;
+
+use chillerlan\HTTP\CurlUtils\{CurlMultiClient, MultiResponseHandlerInterface};
+use chillerlan\HTTP\HTTPOptions;
+use chillerlan\HTTP\Psr7\Request;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\{RequestInterface, ResponseInterface};
+use function chillerlan\HTTP\Psr7\build_http_query;
+
+class CurlMultiClientTest extends TestCase{
+
+	/**
+	 * @var \chillerlan\HTTP\CurlUtils\CurlMultiClient
+	 */
+	protected $http;
+
+	protected function setUp():void{
+
+		$options = new HTTPOptions([
+			'ca_info' => __DIR__.'/../cacert.pem',
+			'sleep'   => 60 / 300 * 1000000,
+		]);
+
+		$this->http = new CurlMultiClient($options);
+	}
+
+	protected function getRequests():array{
+
+		$ids = [
+			[1,2,6,11,15,23,24,56,57,58,59,60,61,62,63,64,68,69,70,71,72,73,74,75,76],
+			[77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101],
+		];
+
+		$requests = [];
+
+		foreach($ids as $chunk){
+			foreach(['de', 'en', 'es', 'fr', 'zh'] as $lang){
+				$requests[] = new Request(Request::METHOD_GET, 'https://api.guildwars2.com/v2/items?'.build_http_query(['lang' => $lang, 'ids' => implode(',', $chunk)]));
+			}
+		}
+
+		return $requests;
+	}
+
+	protected function getTestResponseHandler():MultiResponseHandlerInterface{
+
+		return new class() implements MultiResponseHandlerInterface{
+
+			protected $responses = [];
+
+			public function handleResponse(ResponseInterface $response, RequestInterface $request, int $id, array $curl_info):?RequestInterface{
+
+				if(\in_array($response->getStatusCode(), [200, 206], true)){
+					$this->responses[$id]['lang'] = $response->getHeaderLine('content-language');
+#					\var_dump((string)$response->getBody());
+					// we got the response we expected, return nothing
+					return null;
+				}
+
+				// return the failed request back to the stack
+				return $request;
+			}
+
+			public function getResponses():array{
+				\ksort($this->responses);
+
+				return $this->responses;
+			}
+
+		};
+
+	}
+
+	/**
+	 * @todo
+	 */
+	public function testMultiRequest(){
+		$requests = $this->getRequests();
+		$handler  = $this->getTestResponseHandler();
+
+		$this->http
+			->setMultiResponseHandler($handler)
+			->addRequests($requests)
+			->process()
+		;
+
+		$responses = $handler->getResponses();
+
+		$this->assertCount(10, $requests);
+		$this->assertCount(10, $responses);
+
+		// the responses are ordered
+		$this->assertSame(['de','en','es','fr','zh','de','en','es','fr','zh'], \array_column($responses, 'lang'));
+
+	}
+}
