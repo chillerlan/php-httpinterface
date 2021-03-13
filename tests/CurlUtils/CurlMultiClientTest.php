bf157e7dfb3e487e81d1b5a31de4447937051bd5
codemasher <smiley@chillerlan.net>
2021-03-13 20:35:53 +0100

    :fire: this test *should* run

--- tests/CurlUtils/CurlMultiClientTest.php
+++ tests/CurlUtils/CurlMultiClientTest.php
@@ -95,7 +95,7 @@ class CurlMultiClientTest extends TestCase{
 	 * @todo
 	 */
 	public function testMultiRequest(){
-		$this::markTestIncomplete();
+#		$this::markTestIncomplete();
 		$requests = $this->getRequests();
 
 		$this->http
@@ -110,7 +110,7 @@ class CurlMultiClientTest extends TestCase{
 
 		// the responses are ordered
 		// i'll probably never know why this fails on travis
-#		$this::assertSame(['de', 'en', 'es', 'fr', 'zh', 'de', 'en', 'es', 'fr', 'zh'], array_column($responses, 'lang'));
+		$this::assertSame(['de', 'en', 'es', 'fr', 'zh', 'de', 'en', 'es', 'fr', 'zh'], array_column($responses, 'lang'));
 
 		// cover the destructor
 		unset($this->http);
