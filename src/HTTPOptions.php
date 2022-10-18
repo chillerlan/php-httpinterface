4cb28c616561239bf1897e993f90dd6d69cde522
smiley <codemasher@users.noreply.github.com>
2022-10-18 22:39:54 +0200

    :shower:

--- src/HTTPOptions.php
+++ src/HTTPOptions.php
@@ -13,16 +13,7 @@ namespace chillerlan\HTTP;
 use chillerlan\Settings\SettingsContainerAbstract;
 
 /**
- * @property string    $user_agent
- * @property array     $curl_options
- * @property string    $ca_info
- * @property bool      $ssl_verifypeer
- * @property int       $window_size
- * @property int|float $sleep
- * @property int       $timeout
- * @property int       $retries
- * @property array     $curl_multi_options
- * @property bool      $curl_check_OCSP
+ *
  */
 class HTTPOptions extends SettingsContainerAbstract{
 	use HTTPOptionsTrait;
