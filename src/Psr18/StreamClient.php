cef612ad48a39ffbf841ba98eb69bb1367694070
smiley <codemasher@users.noreply.github.com>
2022-11-01 18:54:52 +0100

    :octocat: StreamClient: now with response headers!

--- src/Psr18/StreamClient.php
+++ src/Psr18/StreamClient.php
@@ -6,12 +6,11 @@
  * @author       smiley <smiley@chillerlan.net>
  * @copyright    2019 smiley
  * @license      MIT
- *
- * @phan-file-suppress PhanTypeInvalidThrowsIsInterface
  */
 
 namespace chillerlan\HTTP\Psr18;
 
+use chillerlan\HTTP\Utils\HeaderUtil;
 use Psr\Http\Message\{RequestInterface, ResponseInterface};
 use function explode, file_get_contents, get_headers, in_array, intval, restore_error_handler,
 	set_error_handler, stream_context_create, strtolower, substr, trim;
@@ -20,39 +19,8 @@ class StreamClient extends HTTPClientAbstract{
 
 	/**
 	 * @inheritDoc
-	 * @throws \Psr\Http\Client\ClientExceptionInterface|\ErrorException
-	 *
 	 */
 	public function sendRequest(RequestInterface $request):ResponseInterface{
-		$uri     = $request->getUri();
-		$method  = $request->getMethod();
-		$headers = $this->getRequestHeaders($request);
-
-		$body = in_array($method, ['DELETE', 'PATCH', 'POST', 'PUT'], true)
-			? $request->getBody()->getContents()
-			: null;
-
-		$context_options = [
-			'http' => [
-				'method'           => $method,
-				'header'           => $headers,
-				'content'          => $body,
-				'protocol_version' => '1.1', // 1.1 is default from PHP 8.0
-				'user_agent'       => $this->options->user_agent,
-				'max_redirects'    => 0,
-				'timeout'          => 5,
-			],
-			'ssl'  => [
-				'verify_peer'         => $this->options->ssl_verifypeer,
-				'verify_depth'        => 3,
-				'peer_name'           => $uri->getHost(),
-				'ciphers'             => 'HIGH:!SSLv2:!SSLv3',
-				'disable_compression' => true,
-			],
-		];
-
-		$ca                          = $this->options->ca_info_is_path ? 'capath' : 'cafile';
-		$context_options['ssl'][$ca] = $this->options->ca_info;
 
 		$errorHandler = function(int $errno, string $errstr):bool{
 			$this->logger->error('StreamClient error #'.$errno.': '.$errstr);
@@ -62,17 +30,12 @@ class StreamClient extends HTTPClientAbstract{
 
 		set_error_handler($errorHandler);
 
-		$context         = stream_context_create($context_options);
-		$requestUri      = (string)$uri->withFragment('');
-		$responseBody    = file_get_contents($requestUri, false, $context);
-		$responseHeaders = $this->parseResponseHeaders(get_headers($requestUri, true, $context));
+		$context      = stream_context_create($this->getContextOptions($request));
+		$requestUri   = (string)$request->getUri()->withFragment('');
+		$responseBody = file_get_contents($requestUri, false, $context);
+		$response     = $this->createResponse(get_headers($requestUri, true, $context));
 
 		restore_error_handler();
-
-		$response = $this->responseFactory
-			->createResponse($responseHeaders['statuscode'], $responseHeaders['statustext'])
-			->withProtocolVersion($responseHeaders['httpversion'])
-		;
 
 		$body = $this->streamFactory !== null
 			? $this->streamFactory->createStream()
@@ -83,6 +46,40 @@ class StreamClient extends HTTPClientAbstract{
 		$body->rewind();
 
 		return $response->withBody($body);
+	}
+
+	/**
+	 *
+	 */
+	protected function getContextOptions(RequestInterface $request):array{
+		$method = $request->getMethod();
+		$body   = in_array($method, ['DELETE', 'PATCH', 'POST', 'PUT'], true)
+			? $request->getBody()->getContents()
+			: null;
+
+		$options = [
+			'http' => [
+				'method'           => $method,
+				'header'           => $this->getRequestHeaders($request),
+				'content'          => $body,
+				'protocol_version' => '1.1', // 1.1 is default from PHP 8.0
+				'user_agent'       => $this->options->user_agent,
+				'max_redirects'    => 0,
+				'timeout'          => 5,
+			],
+			'ssl'  => [
+				'verify_peer'         => $this->options->ssl_verifypeer,
+				'verify_depth'        => 3,
+				'peer_name'           => $request->getUri()->getHost(),
+				'ciphers'             => 'HIGH:!SSLv2:!SSLv3',
+				'disable_compression' => true,
+			],
+		];
+
+		$ca                  = $this->options->ca_info_is_path ? 'capath' : 'cafile';
+		$options['ssl'][$ca] = $this->options->ca_info;
+
+		return $options;
 	}
 
 	/**
@@ -107,25 +104,38 @@ class StreamClient extends HTTPClientAbstract{
 	/**
 	 * @param string[] $headers
 	 */
-	protected function parseResponseHeaders(array $headers):array{
+	protected function createResponse(array $headers):ResponseInterface{
 		$h = [];
+
+		$httpversion = null;
+		$statuscode  = null;
+		$statustext  = null;
 
 		foreach($headers as $k => $v){
 
 			if($k === 0 && substr($v, 0, 4) === 'HTTP'){
 				$status = explode(' ', $v, 3);
 
-				$h['httpversion'] = explode('/', $status[0], 2)[1];
-				$h['statuscode']  = intval($status[1]);
-				$h['statustext']  = trim($status[2]);
+				$httpversion = explode('/', $status[0], 2)[1];
+				$statuscode  = intval($status[1]);
+				$statustext  = trim($status[2]);
 
 				continue;
 			}
 
-			$h[strtolower($k)] = $v;
+			$h[$k] = $v;
 		}
 
-		return $h;
+		$response = $this->responseFactory
+			->createResponse($statuscode, $statustext)
+			->withProtocolVersion($httpversion)
+		;
+
+		foreach(HeaderUtil::normalize($h) as $k => $v){
+			$response = $response->withAddedHeader($k, $v);
+		}
+
+		return $response;
 	}
 
 }
