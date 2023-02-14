f6b4f4d967a7879684e3433883e847e221659966
smiley <codemasher@users.noreply.github.com>
2023-02-14 03:03:24 +0100

    :sparkles: extract HTTPClientInterface

--- src/Psr18/HTTPClientAbstract.php
+++ src/Psr18/HTTPClientAbstract.php
@@ -11,50 +11,60 @@
 namespace chillerlan\HTTP\Psr18;
 
 use chillerlan\HTTP\HTTPOptions;
-use Psr\Http\Message\StreamFactoryInterface;
 use chillerlan\HTTP\Psr17\{ResponseFactory};
 use chillerlan\Settings\SettingsContainerInterface;
-use Fig\Http\Message\RequestMethodInterface;
-use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Log\{LoggerAwareInterface, LoggerAwareTrait, LoggerInterface, NullLogger};
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Log\{LoggerInterface, NullLogger};
 
-abstract class HTTPClientAbstract implements ClientInterface, LoggerAwareInterface, RequestMethodInterface{
-	use LoggerAwareTrait;
+abstract class HTTPClientAbstract implements HTTPClientInterface{
 
-	/** @var \chillerlan\Settings\SettingsContainerInterface|\chillerlan\HTTP\HTTPOptions */
-	protected SettingsContainerInterface $options;
-
-	protected ResponseFactoryInterface $responseFactory;
-	protected ?StreamFactoryInterface $streamFactory = null;
+	protected HTTPOptions|SettingsContainerInterface $options;
+	protected LoggerInterface                        $logger;
+	protected ResponseFactoryInterface               $responseFactory;
+	protected ?StreamFactoryInterface                $streamFactory   = null;
 
 	/**
 	 * HTTPClientAbstract constructor.
 	 */
 	public function __construct(
-		SettingsContainerInterface $options = null,
+		HTTPOptions|SettingsContainerInterface $options = null,
 		ResponseFactoryInterface $responseFactory = null,
 		LoggerInterface $logger = null
 	){
 		$this->options = $options ?? new HTTPOptions;
 
-		$this->setResponseFactory($responseFactory ?? new ResponseFactory);
-		$this->setLogger($logger ?? new NullLogger);
+		$this
+			->setResponseFactory($responseFactory ?? new ResponseFactory)
+			->setLogger($logger ?? new NullLogger)
+		;
 	}
 
 	/**
+	 * @inheritDoc
 	 * @codeCoverageIgnore
 	 */
-	public function setResponseFactory(ResponseFactoryInterface $responseFactory):self{
+	public function setLogger(LoggerInterface $logger):HTTPClientInterface{
+		$this->logger = $logger;
+
+		return $this;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @codeCoverageIgnore
+	 */
+	public function setResponseFactory(ResponseFactoryInterface $responseFactory):HTTPClientInterface{
 		$this->responseFactory = $responseFactory;
 
 		return $this;
 	}
 
 	/**
+	 * @inheritDoc
 	 * @codeCoverageIgnore
 	 */
-	public function setStreamFactory(StreamFactoryInterface $streamFactory):self{
+	public function setStreamFactory(StreamFactoryInterface $streamFactory):HTTPClientInterface{
 		$this->streamFactory = $streamFactory;
 
 		return $this;
---
+++ src/Psr18/HTTPClientInterface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Interface HTTPClientInterface
+ *
+ * @created      14.02.2023
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2023 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\HTTP\Psr18;
+
+use Fig\Http\Message\RequestMethodInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ *
+ */
+interface HTTPClientInterface extends ClientInterface, RequestMethodInterface{
+
+	/**
+	 * Sets a PSR-3 Logger
+	 */
+	public function setLogger(LoggerInterface $logger):HTTPClientInterface;
+
+	/**
+	 * Sets a PSR-17 response factory
+	 */
+	public function setResponseFactory(ResponseFactoryInterface $responseFactory):HTTPClientInterface;
+
+	/**
+	 * Sets a PSR-17 stream factory
+	 */
+	public function setStreamFactory(StreamFactoryInterface $streamFactory):HTTPClientInterface;
+
+}
